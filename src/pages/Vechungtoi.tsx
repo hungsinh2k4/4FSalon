814ccf1d8d1a9f9ae18a428ec88abd5ccbe48e3b
Nguyễn Sinh Hùng <monternsh@gmail.com>
2024-10-26 15:44:46 +0700

    add lien he ...

---
+++ src/pages/Vechungtoi.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Vechungtoi: React.FC = () => {
+    return (
+        <h1 className="text-center text-2xl "> Về chúng tôi</h1>
+    )
+}
+export default Vechungtoi;
