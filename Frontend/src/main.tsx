087a379ef35e9bed17c55b752f6f25719983ab21
bufi.hofa <hoapri123@gmail.com>
2024-10-30 21:47:14 +0700

    edit route

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './customer/App';
+import ManagerApp from './manager/ManagerApp';
 import './index.css';
 
+//main.tsx
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/manager/*" element={<ManagerApp />} />
+        <Route path="/*" element={<App />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
 );
