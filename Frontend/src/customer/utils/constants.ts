426794ef8db9241da9d540fab2a4a8963de961f9
Nguyễn Sinh Hùng <monternsh@gmail.com>
2024-11-03 16:41:14 +0700

    Create axios

---
+++ Frontend/src/customer/utils/constants.ts
@@ -0,0 +1,4 @@
+// src/customer/utils/constants.ts
+export const API_BASE_URL =
+  "https://repulsive-emily-vietnam-national-university-1d0f7e29.koyeb.app"; // Thay thế bằng URL thực tế của bạn
+// Các hằng số khác nếu cần
