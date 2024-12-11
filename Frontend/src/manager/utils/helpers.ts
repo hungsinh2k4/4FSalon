c020d4ae4aa4a89d7d4117fef4c7501f29b002c1
lyhduc <lyhongduc.34@gmail.com>
2024-12-11 21:37:42 +0700

    Feat: Statistic and dashboard

--- Frontend/src/manager/utils/helpers.ts
+++ Frontend/src/manager/utils/helpers.ts
@@ -5,7 +5,7 @@ export const formatDate = (date: string) => {
   };
   
 export const formatMoney = (amount: number) => {
-  if (!amount) return 'Trống';
+  if (!amount) return '0đ';
   return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(amount);
 }
   // Các helper functions khác nếu cần
